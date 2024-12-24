1aca6ca7d2fe840b65a38d17b002e9b1e542b8be
thintkn <ntkathie@gmail.com>
2024-12-24 22:05:38 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 document.getElementById("play-music").addEventListener("click", function() {
     const music = document.getElementById("christmas-music");
+
     if (music.paused) {
-        music.play();
-        this.textContent = "🎶 Pause Music";
+        music.play();  
+        this.textContent = "🎶 Pause Music"; 
     } else {
-        music.pause();
-        this.textContent = "🎶 Play Christmas Music";
+        music.pause();  
+        this.textContent = "🎶 Play Christmas Music";  
     }
 });
