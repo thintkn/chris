464da2a8fd96c7ec455f48373425eb2e1325b875
thintkn <ntkathie@gmail.com>
2024-12-24 21:26:49 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+document.getElementById("play-music").addEventListener("click", function() {
+    const music = document.getElementById("christmas-music");
+    if (music.paused) {
+        music.play();
+        this.textContent = "🎶 Pause Music";
+    } else {
+        music.pause();
+        this.textContent = "🎶 Play Christmas Music";
+    }
+});
